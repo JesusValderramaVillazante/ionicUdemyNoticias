6891dbba80f6acd0579ef42b1556d7f4a1a25956
jesus <imangelo@hotmail.es>
2019-03-19 00:48:10 +0100

    interfaces para manejo de informacion

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RespuestaTopHeadLines } from '../interfaces/interfaces';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,6 +9,6 @@ export class NoticiasService {
   constructor(private http: HttpClient) { }
 
   getTopHeadLines() {
-    return this.http.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=d347ef5ba07b4f0e84004a5101df02b8`);
+    return this.http.get<RespuestaTopHeadLines>(`https://newsapi.org/v2/top-headlines?country=us&apiKey=d347ef5ba07b4f0e84004a5101df02b8`);
   }
 }

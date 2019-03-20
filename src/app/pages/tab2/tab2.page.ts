8f67c2d03c662a5c4fdef9ee1660e4ceee93356b
jesus <imangelo@hotmail.es>
2019-03-20 23:51:47 +0100

    pagina de encabezado

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page implements OnInit{
+  categorias = ['entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+  
+  ngOnInit(){
+  }
+
+  segmentChanged(e) {
+    console.log(e.detail);
+  }
+}
